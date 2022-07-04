8045b17b67ed187551603c49e2f40b54cbcfb10b
eneritz5 <enebu.eb@gmail.com>
2022-07-04 15:17:23 +0200

    warning img

--- src/components/CharacterHome/EachCharacterHome.js
+++ src/components/CharacterHome/EachCharacterHome.js
@@ -5,7 +5,7 @@ export const EachCharacter = (eachCharacter) => {
     return (
         <Link to={`/${eachCharacter.character.char_id}`} element={<Character character={eachCharacter} />} character={eachCharacter.character} className="decoration-none">
             <div key={eachCharacter.character.char_id} className='character_card'>
-                <img className='character_img' src={eachCharacter.character.img} alt="character image" />
+                <img className='character_img' src={eachCharacter.character.img} alt="character breaking bad" />
                 <h4 className='character_name'>{eachCharacter.character.name}</h4>
             </div>
         </Link>
